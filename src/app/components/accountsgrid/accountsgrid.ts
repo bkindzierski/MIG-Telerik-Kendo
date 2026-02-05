6f9518aed7e571e4d8a75f5d67e51032be0793be
bkindzierski@merchantsgroup.com <bkindzierski@merchantsgroup.com>
2026-02-05 17:21:30 -0500

    scss updates

--- src/app/components/accountsgrid/accountsgrid.ts
+++ src/app/components/accountsgrid/accountsgrid.ts
@@ -1,12 +1,19 @@
 import { Component, computed, effect, input, OnInit, signal } from '@angular/core'
-import { CommonModule } from '@angular/common';
+import { CommonModule, AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs/internal/Observable';
+import { Subscription } from 'rxjs';
 import { Account } from '../../../app/model/account.model'
 import { AccountDataService } from '../../../../src/services/account-data.service';
 import { FormsModule } from '@angular/forms';
+import { KENDO_LABELS } from "@progress/kendo-angular-label";
+import { GridComponent, GridDataResult, KENDO_GRID, SelectableSettings, SelectionEvent } from '@progress/kendo-angular-grid'
+import { KENDO_SVGICON } from '@progress/kendo-angular-icons';
+import { editToolsIcon } from '@progress/kendo-svg-icons';
+import { State, process } from '@progress/kendo-data-query';
 
 @Component({
   selector: 'app-accountsgrid',
-  imports: [CommonModule],
+  imports: [CommonModule,KENDO_LABELS,KENDO_GRID,KENDO_SVGICON, AsyncPipe],
   templateUrl: './accountsgrid.html',
   styleUrl: './accountsgrid.scss',
   providers: [AccountDataService],
@@ -15,7 +22,19 @@ export class Accountsgrid {
 
   // Account[];  
   #accounts = signal<Account[]>([]);
-  
+  private serviceSubscription: Subscription;
+  //public gridAccounts$!: Observable<Account[]>; // Data source as an Observable
+  public view: Observable<GridDataResult>;
+  public editIcon = editToolsIcon;
+
+  public gridData: GridDataResult | undefined
+  /** GRID state */
+  public gridState: State = {
+    skip:0,
+    take:10,
+    sort:[],
+    group:[]
+  }
 
   constructor(private accountService: AccountDataService){
     this.loadAccounts();
@@ -28,8 +47,20 @@ export class Accountsgrid {
   ngOnInit(){
 
   }
+  public onDataStateChange(newState: State){
+    this.gridState = newState;
+    this.loadAccounts();
+  }
 
+  public onSelectionChange(args: SelectionEvent){
+    const cellItem =  args.selectedRows[0].dataItem as any
+    //console.log('args', cellItem.storeId);
+  }
 
+  public selectableSettings: SelectableSettings = { 
+    //checkboxOnly: false
+  };
+  
   async loadAccounts(){ 
     try {
         // const accounts = await this.accountService.getAccountData(); 
@@ -46,9 +77,19 @@ export class Accountsgrid {
   }
   
   getAccounts = computed(() => {
-    const accounts = this.#accounts();   
-    return accounts.filter(acct=>acct.version == "6.1");    
+    // const accounts = this.#accounts();   
+    // return accounts.filter(acct=>acct.version == "6.1");   
+    return this.gridData = process(this.#accounts(), this.gridState);
   });
 
+  public getIndicatorImage(item:any) : string{
+    //return ItemType.getIndicatorImage(item.type)
+    return '';
+  }
 
+  public  ngOnDestroy(): void {
+    if (this.serviceSubscription) {
+      this.serviceSubscription.unsubscribe();
+    }    
+  }
 }
