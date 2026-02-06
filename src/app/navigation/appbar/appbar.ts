4ba17b689217e6c153f8072b60046a7f1a739e94
bkindzierski@merchantsgroup.com <bkindzierski@merchantsgroup.com>
2026-02-06 14:53:57 -0500

    added correct import for svg icons

--- src/app/navigation/appbar/appbar.ts
+++ src/app/navigation/appbar/appbar.ts
@@ -4,7 +4,7 @@ import { KENDO_ICONS } from "@progress/kendo-angular-icons";
 import { KENDO_INDICATORS } from "@progress/kendo-angular-indicators";
 import { KENDO_LAYOUT } from "@progress/kendo-angular-layout";
 import { KENDO_NAVIGATION } from "@progress/kendo-angular-navigation";
-//import { bellIcon, menuIcon, SVGIcon, userIcon } from "@progress/kendo-svg-icon";
+import { bellIcon, menuIcon, SVGIcon, userIcon } from "@progress/kendo-svg-icons";
 import { KENDO_LABEL } from '@progress/kendo-angular-label';
 
 @Component({
@@ -15,9 +15,9 @@ import { KENDO_LABEL } from '@progress/kendo-angular-label';
 })
 export class Appbar {
 
-  // public menuIcon: SVGIcon = menuIcon;
-  // public bellIcon: SVGIcon = bellIcon;
-  // public userIcon: SVGIcon = userIcon;
+  public menuIcon: SVGIcon = menuIcon;
+  public bellIcon: SVGIcon = bellIcon;
+  public userIcon: SVGIcon = userIcon;
 
   constructor(){
   }
